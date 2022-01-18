df3ca227a42caa69ddd6b08443e909532a20b0e2
Lucas Garcia <lucasgarcia561@gmail.com>
2022-01-18 16:40:53 -0300

    ajustes no menu mobile, feito o efeito esconder quando clicado

--- assests/js/style.js
+++ assests/js/style.js
@@ -11,3 +11,22 @@ const showMenu = (toggleId, navId) => {
 }
 
 showMenu('nav-toggle', 'nav')
+
+/*removendo menu mobile*/
+const navLink = document.querySelectorAll('.nav_link')
+function linkAction() {
+    const navMenu = document.getElementById('nav')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*deixando o link clicado com a class active*/
+const linkColor = document.querySelectorAll('.nav_link')
+function colorLink() {
+    if(linkColor){
+        linkColor.forEach(L => L.classList.remove('active-link'))
+        this.classList.add('active-link')
+    }
+}
+
+linkColor.forEach(L=> L.addEventListener('click', colorLink))
